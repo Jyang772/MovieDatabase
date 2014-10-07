c77faf3f3f1959813412b42c097119fa5e099c4a
Justin <jyang772@gmail.com>
2014-10-07 14:25:45 -0700

    Changed

--- SortedMovieList.cpp
+++ SortedMovieList.cpp
@@ -10,10 +10,21 @@
 SortedMovieList::SortedMovieList() : size(0){}
 
 bool SortedMovieList::isEmpty() const{
+    // Determines whether a list is empty.
+    // Precondition: None.
+    // Postcondition: Returns true if the list is empty;
+    // otherwise returns false.
+
+
     return(size == 0 ? 1 : 0);
 }
 
 int SortedMovieList::getLength() const{
+
+        // Determines the length of a list.
+        // Precondition: None.
+        // Postcondition: Returns the number of items
+        // that are currently in the list.
     return size;
 }
 
@@ -28,6 +39,17 @@ bool SortedMovieList::remove(int index){
 }
 
 bool SortedMovieList::insert(int index, ListItemType newItem){
+        // Inserts an item into the list at position index.
+        // Precondition: index indicates the position at which
+        // the item should be inserted in the list.
+        // Postcondition: If insertion is successful, newItem is
+        // at position index in the list, and other items are
+        // renumbered accordingly, and true is returned;
+        // otherwise false is returned.
+        // Note: Insertion will not be successful if
+        // index < 0 or index > getLength().
+
+
     //Insert MovieType structure into list
 
     if(size+1 > MAX_LIST || index < 0)
@@ -57,11 +79,11 @@ bool SortedMovieList::add(const ListItemType &newItem){
 
     for(int i=0; i<size;i++){
 
-//        if(items[i].compareKeys(newItem) > 0 && (i==0)){
-//            index = 0;
-//            cout << "newItem1: " << newItem.m_title << endl;
-//        }
-         if(items[i].compareKeys(newItem) > 0){
+        //        if(items[i].compareKeys(newItem) > 0 && (i==0)){
+        //            index = 0;
+        //            cout << "newItem1: " << newItem.m_title << endl;
+        //        }
+        if(items[i].compareKeys(newItem) > 0){
             index = i;
             break;
         }
@@ -98,9 +120,6 @@ bool SortedMovieList::retrieve(int index, ListItemType &dataItem) const{
     return true;
 
 }
-
-
-
 
 
 //Overloaded functions
@@ -151,7 +170,15 @@ void SortedMovieList::remove(string name){
 
 }
 
+
 void SortedMovieList::retrieve(string name) const{
+        // Retrieves a list item by position.
+       // Precondition: index is the number of the item to
+       // be retrieved.
+       // Postcondition: If 0 <= index < getLength(),
+       // dataItem is the value of the desired item and
+       // true is returned; otherwise false is returned.
+
 
     string compare;
     unsigned int match = 0;
@@ -171,33 +198,33 @@ void SortedMovieList::retrieve(string name) const{
     }
 
 
-//    /*****/
-//    /*Binary Search*/
+    //    /*****/
+    //    /*Binary Search*/
 
-//    cout << "size: " << size << endl;
-//    int mid = size/2;
-//    int first = 0, last = size;
+    //    cout << "size: " << size << endl;
+    //    int mid = size/2;
+    //    int first = 0, last = size;
 
-//    while(true){
+    //    while(true){
 
-//        compare.resize(items[mid].m_title.length());
-//        transform(items[mid].m_title.begin(),items[mid].m_title.end(),compare.begin(),::tolower);
+    //        compare.resize(items[mid].m_title.length());
+    //        transform(items[mid].m_title.begin(),items[mid].m_title.end(),compare.begin(),::tolower);
 
-//        if(compare.compare(name) == 0){
-//            display(items[mid]);
-//            return;
-//        }
-//        else if(name.compare(compare) < 0)
-//            last = mid - 1;
-//        else
-//            first = mid + 1;
+    //        if(compare.compare(name) == 0){
+    //            display(items[mid]);
+    //            return;
+    //        }
+    //        else if(name.compare(compare) < 0)
+    //            last = mid - 1;
+    //        else
+    //            first = mid + 1;
 
-//        if(first > last)
-//            break;
+    //        if(first > last)
+    //            break;
 
-//        mid = (first + last)/2;
-//    }
-//    /****/
+    //        mid = (first + last)/2;
+    //    }
+    //    /****/
 
 
     cout << "\nMovie Not Found. Displaying similar results:\n" << endl;
@@ -218,10 +245,11 @@ void SortedMovieList::retrieve(string name) const{
             else if(match == name.length()/2){
                 display(items[i]);
                 break;}
-            }
         }
+    }
 
 }
+
 
 void SortedMovieList::display(const ListItemType &dataItem) const{
 
